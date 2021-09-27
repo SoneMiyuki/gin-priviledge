3819d2a438a1ccd55e19ebf345b3dc8dbb989f39
GilbertTrumpTrunks <1191698255@qq.com>
2021-09-28 00:28:53 +0800

    frontend

---
+++ frontend/gin-vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      <b-container>
+        <navbar />
+      </b-container>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from './views/layout/Navbar.vue';
+
+export default {
+  components: { Navbar },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
